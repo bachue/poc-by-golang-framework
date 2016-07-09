49f5d88a17928562cee591f9ddc01a1e55172437
Bachue Zhou <bachue.shu@gmail.com>
2016-07-09 15:17:05 +0800

    add back the -session-count

--- test-maweiwei.go
+++ test-maweiwei.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync/atomic"
 	"time"
 
 	"github.com/tuvistavie/securerandom"
@@ -28,9 +29,10 @@ type Doc map[string]string
 type Server struct {
 	debug       bool
 	verbose     bool
+	idx         uint32
 	keyCount    int
 	valueLength int
-	coll        *mgo.Collection
+	colls       []*mgo.Collection
 	samples     []Doc
 }
 
@@ -61,7 +63,7 @@ func (s *Server) find(w http.ResponseWriter, r *http.Request) {
 		log.Println("Query:", sample)
 	}
 
-	n, err := s.coll.Find(sample).Count()
+	n, err := s.getCollection().Find(sample).Count()
 	if err != nil {
 		log.Println("Find from db failed", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -79,7 +81,7 @@ func (s *Server) insert(w http.ResponseWriter, r *http.Request) {
 	for i := 0; i < s.keyCount; i++ {
 		doc[fmt.Sprintf("key%v", i)] = RandomString(s.valueLength)
 	}
-	err := s.coll.Insert(doc)
+	err := s.getCollection().Insert(doc)
 	if err != nil {
 		log.Println("Insert to db failed", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -92,7 +94,7 @@ func (s *Server) createSamples(w http.ResponseWriter, r *http.Request) {
 	params := r.URL.Query()
 
 	for i := 0; i < 20; i++ {
-		err := s.coll.EnsureIndexKey("key" + strconv.Itoa(i))
+		err := s.getCollection().EnsureIndexKey("key" + strconv.Itoa(i))
 		if err != nil {
 			log.Println("Failed to ensure index", err)
 			w.WriteHeader(http.StatusInternalServerError)
@@ -110,7 +112,7 @@ func (s *Server) createSamples(w http.ResponseWriter, r *http.Request) {
 		aggregation := []bson.M{bson.M{"$sample": bson.M{"size": count}}}
 
 		var results []Doc
-		err = s.coll.Pipe(aggregation).All(&results)
+		err = s.getCollection().Pipe(aggregation).All(&results)
 		if err != nil {
 			log.Println("Failed to create samples", err)
 			w.WriteHeader(http.StatusInternalServerError)
@@ -129,6 +131,11 @@ func (s *Server) createSamples(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 }
 
+func (s *Server) getCollection() *mgo.Collection {
+	id := atomic.AddUint32(&s.idx, 1) % uint32(len(s.colls))
+	return s.colls[id]
+}
+
 func main() {
 	mgoAddr := flag.String("addr", "127.0.0.1", "mongodb addr")
 	db := flag.String("db", "poc-go", "db")
@@ -136,6 +143,7 @@ func main() {
 	listenAddr := flag.String("listen", ":9876", "server listen addr")
 	keyCount := flag.Int("count", 20, "key count")
 	valueLength := flag.Int("value", 128, "value length")
+	sessionCount := flag.Int("session-count", 20, "Mongodb Session Count")
 	verbose := flag.Bool("verbose", false, "verbose mode")
 	debug := flag.Bool("debug", false, "debug mode")
 	flag.Parse()
@@ -151,17 +159,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer s.Close()
 	s.SetMode(mgo.Nearest, true)
 	s.SetPoolLimit(1048560)
+
+	sessions := make([]*mgo.Session, *sessionCount)
+	sessions[0] = s
+	for i := 1; i < *sessionCount; i++ {
+		sessions[i] = s.Copy()
+	}
 
 	server := &Server{
 		verbose:     *verbose,
 		debug:       *debug,
 		keyCount:    *keyCount,
 		valueLength: *valueLength,
+		colls:       make([]*mgo.Collection, *sessionCount),
 	}
-	server.coll = s.DB(*db).C(*coll)
+	for i, session := range sessions {
+		defer session.Close()
+		server.colls[i] = session.DB(*db).C(*coll)
+	}
 	http.HandleFunc("/", server.Root)
 	log.Fatal(http.ListenAndServe(*listenAddr, nil))
 }
