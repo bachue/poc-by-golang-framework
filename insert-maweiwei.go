811027a854bd8fcc9214c97bfc6d724588870b43
Bachue Zhou <bachue.shu@gmail.com>
2016-07-11 00:34:44 +0800

    creating sample done

--- insert-maweiwei.go
+++ insert-maweiwei.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"math/rand"
 	"runtime"
+	"strconv"
 	"sync/atomic"
 	"time"
 
@@ -19,10 +22,14 @@ var (
 	db              = flag.String("d", "test", "db")
 	coll            = flag.String("c", "test", "coll")
 	dbCount         = flag.Int("dbs", 1, "db count")
-	amount          = flag.Uint64("a", 0, "number of data")
-	total           = uint64(0)
+	writeCount      = flag.Uint64("qw", 0, "number of write")
+	queryCount      = flag.Uint64("qr", 0, "number of query")
+	sampleCount     = flag.Uint64("qs", 2000, "number of samples")
+	totalWrite      = uint64(0)
+	totalQuery      = uint64(0)
 	last            = time.Now()
 	collsList       [][]*mgo.Collection
+	samples         []map[string]string
 )
 
 func generateSecureRandomHex(n int) string {
@@ -36,7 +43,7 @@ func generateSecureRandomHex(n int) string {
 func write(colls []*mgo.Collection, done chan<- bool) {
 	var t uint64
 	for {
-		if t = atomic.AddUint64(&total, 1); *amount > 0 && t > *amount {
+		if t = atomic.AddUint64(&totalWrite, 1); *writeCount > 0 && t > *writeCount {
 			break
 		}
 
@@ -75,6 +82,44 @@ func write(colls []*mgo.Collection, done chan<- bool) {
 	done <- true
 }
 
+func readSamples(coll *mgo.Collection) {
+	var (
+		count   uint64
+		rest    uint64 = *sampleCount
+		results []map[string]string
+		sample  map[string]string
+		key     string
+	)
+	for {
+		if rest >= 2000 {
+			count = 2000
+		} else {
+			count = rest
+		}
+
+		aggregation := []bson.M{bson.M{"$sample": bson.M{"size": count}}}
+		err := coll.Pipe(aggregation).All(&results)
+		if err != nil {
+			panic(err)
+		}
+		for _, result := range results {
+			sample = make(map[string]string)
+			for j := 0; j < rand.Intn(5)+1; j++ {
+				key = "key" + strconv.Itoa(rand.Intn(20))
+				sample[key] = result[key]
+			}
+			fmt.Printf("sample: %v\n", sample)
+			samples = append(samples, sample)
+		}
+
+		if rest > 2000 {
+			rest -= 2000
+		} else {
+			break
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -97,11 +142,18 @@ func main() {
 		done[i] = make(chan bool, 1)
 	}
 
-	for i := 0; i < *NumberGoroutine; i++ {
-		go write(collsList[i], done[i])
+	if *writeCount > 0 {
+		for i := 0; i < *NumberGoroutine; i++ {
+			go write(collsList[i], done[i])
+		}
+
+		for i := 0; i < *NumberGoroutine; i++ {
+			<-done[i]
+		}
 	}
 
-	for i := 0; i < *NumberGoroutine; i++ {
-		<-done[i]
+	if *queryCount > 0 {
+		samples = make([]map[string]string, 0, *sampleCount)
+		readSamples(collsList[0][0])
 	}
 }
